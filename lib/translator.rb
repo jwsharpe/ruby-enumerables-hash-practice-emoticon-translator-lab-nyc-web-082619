494aab9b081425999b4c0086c8c7ee0a2959e0f6
James Sharpe <jameswrightsharpe@gmail.com>
2019-08-05 00:36:17 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,11 +15,11 @@ $emoticons.each_key do |key|
   $english_to_japanese_dictionary = {emoticons[key][0] => emoticons[key][1]}
 
 def get_japanese_emoticon(emoticon)
-  
+  return $english_to_japanese_dictionary[emoticon]
 end
 
 def get_english_meaning(emoticon)
-  
+  return $japanese_to_english_meaning_dictionary[emoticon]
 end
 
 
