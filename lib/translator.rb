96729da1b1bf2e44aeb2d2729278fb4b335a825d
James Sharpe <jameswrightsharpe@gmail.com>
2019-08-05 01:14:30 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,14 +2,13 @@ require "yaml"
 
 def load_library(file_path)
   yaml_hash = YAML.load_file(file_path)
-  dictionary_hash = Hash.new
-  dictionary_hash[:get_meaning] = Hash.new
-  dictionary_hash[:get_emoticon] = Hash.new
+  dictionary_hash = {get_meaning: nil, get_emoticon: nil}
+  
   yaml_hash.each_key do |key|
     dictionary_hash[:get_meaning][yaml_hash[key][1]] = key
     dictionary_hash[:get_emoticon][yaaml_hash[key[0]]] = yaml_hash[key][1]
   end
-  pp dictionary_hash
+  dictionary_hash
 end
 
 def get_japanese_emoticon(yaml_file,emoticon)
