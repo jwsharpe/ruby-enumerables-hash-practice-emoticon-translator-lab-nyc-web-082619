33011e7481bb4ad779e58d12010430d1bc07d1b5
James Sharpe <jameswrightsharpe@gmail.com>
2019-08-05 00:50:14 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,6 +7,7 @@ end
 def get_japanese_emoticon(yaml_file,emoticon)
   emoticons = load_library(yaml_file)
   english_to_japanese_dictionary = Hash.new
+  english_to_japanese_dictionary.default = "I am sorry, I am not sure."
   
   emoticons.each_key do |key|
     english_to_japanese_dictionary = {emoticons[key][0] => emoticons[key][1]}
@@ -18,6 +19,7 @@ end
 def get_english_meaning(emoticon)
   emoticons = load_library(yaml_file)
   japanese_to_english_meaning_dictionary = Hash.new
+  japanese_to_english_meaning_dictionary.default = "I am sorry, I am not sure."
   
   emoticons.each_key do |key|
     japanese_to_english_meaning_dictionary = {$emoticons[key][1] => key}
