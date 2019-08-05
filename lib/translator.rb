d401f5e6c2e6b5645c21515711a68127be4cbe9f
James Sharpe <jameswrightsharpe@gmail.com>
2019-08-05 01:10:26 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,11 +2,13 @@ require "yaml"
 
 def load_library(file_path)
   yaml_hash = YAML.load_file(file_path)
+  dictionary_hash = Hash.new
   dictionary_hash[:get_meaning] = []
   dictionary_hash[:get_emoticon] = []
   yaml_hash.each_key do |key|
     pp key
     dictionary_hash[get_meaning].push({yaml_hash[key][1] => key})
+    
   end
   pp dictionary_hash
 end
