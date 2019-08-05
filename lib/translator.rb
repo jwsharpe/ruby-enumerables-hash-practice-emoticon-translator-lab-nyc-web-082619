0bf5104958a713c96f4cc103bcf52e96096560c5
James Sharpe <jameswrightsharpe@gmail.com>
2019-08-05 01:01:20 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,8 +5,8 @@ def load_library(file_path)
   dictionary_hash = Hash.new
   yaml_hash.each_key do |key|
     dictionary_hash = {
-      get_meaning: key,
-      get_emoticon: key 
+      get_meaning: yaml_hash[key],
+      get_emoticon: yaml_hash[key] 
     }
   end
   return dictionary_hash
