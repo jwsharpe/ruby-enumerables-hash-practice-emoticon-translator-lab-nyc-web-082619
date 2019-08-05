a3e6f6f234b69ae711e9284a32c11741153bdb39
James Sharpe <jameswrightsharpe@gmail.com>
2019-08-05 00:47:24 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,9 +1,7 @@
 require "yaml"
 
-$emoticons = []
-
 def load_library(file_path)
-$emoticons = YAML.load_file(file_path)
+YAML.load_file(file_path)
 end
 
 $emoticons = load_library()
@@ -15,8 +13,13 @@ $emoticons.each_key do |key|
   $english_to_japanese_dictionary = {$emoticons[key][0] => $emoticons[key][1]}
 end
 
-def get_japanese_emoticon(emoticon)
-  return $english_to_japanese_dictionary[emoticon]
+def get_japanese_emoticon(yaml_file,emoticon)
+  emoticons = load_library(yaml_file)
+  english_to_japanese_dictionary = Hash.new
+  emoticons.each_key do |key|
+   english_to_japanese_dictionary = {$emoticons[key][0] => $emoticons[key][1]}
+end
+  return english_to_japanese_dictionary[emoticon]
 end
 
 def get_english_meaning(emoticon)
