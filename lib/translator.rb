f51e631d25d1b316fe50e149a234d4e5f9c5f925
James Sharpe <jameswrightsharpe@gmail.com>
2019-08-05 00:49:04 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,24 +4,24 @@ def load_library(file_path)
 YAML.load_file(file_path)
 end
 
-$emoticons = load_library()
-$japanese_to_english_meaning_dictionary = Hash.new
-$english_to_japanese_dictionary = Hash.new
-
-$emoticons.each_key do |key|
-  $japanese_to_english_meaning_dictionary = {$emoticons[key][1] => key}
-  $english_to_japanese_dictionary = {$emoticons[key][0] => $emoticons[key][1]}
-end
-
 def get_japanese_emoticon(yaml_file,emoticon)
   emoticons = load_library(yaml_file)
   english_to_japanese_dictionary = Hash.new
+  
   emoticons.each_key do |key|
-   english_to_japanese_dictionary = {$emoticons[key][0] => $emoticons[key][1]}
-end
+    english_to_japanese_dictionary = {emoticons[key][0] => emoticons[key][1]}
+  end
+  
   return english_to_japanese_dictionary[emoticon]
 end
 
 def get_english_meaning(emoticon)
-  return $japanese_to_english_meaning_dictionary[emoticon]
+  emoticons = load_library(yaml_file)
+  japanese_to_english_meaning_dictionary = Hash.new
+  
+  emoticons.each_key do |key|
+    japanese_to_english_meaning_dictionary = {$emoticons[key][1] => key}
+  end
+  
+  return japanese_to_english_meaning_dictionary[emoticon]
 end
