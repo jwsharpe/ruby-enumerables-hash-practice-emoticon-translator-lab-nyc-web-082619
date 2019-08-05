048d59934375f15b2e2171aea2802a0f374869ec
James Sharpe <jameswrightsharpe@gmail.com>
2019-08-05 00:59:41 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,14 @@
 require "yaml"
 
 def load_library(file_path)
-  YAML.load_file(file_path)
+  yaml_hash = YAML.load_file(file_path)
+  dictionary_hash = Hash.new
+  yaml_hash.each_key do |key|
+    dictionary_hash = {
+      get_meaning: nil,
+      get_emoticon: nil 
+    }
+  end
 end
 
 def get_japanese_emoticon(yaml_file,emoticon)
